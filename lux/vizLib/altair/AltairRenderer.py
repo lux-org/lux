c99a1b977a0904e042c757675e610a7e5607de5a
Doris Lee <dorisjunglinlee@gmail.com>
2020-01-14 14:17:09 -0800

    various bug fixes for the demo

--- lux/vizLib/altair/AltairRenderer.py
+++ lux/vizLib/altair/AltairRenderer.py
@@ -26,6 +26,6 @@ class AltairRenderer:
 		chart = chart.chart.to_dict()
 		# del chart["datasets"]
 		# chart["data"] =  { "name": 'chartData' }
-		chart["width"] = 180
+		chart["width"] = 160
 		chart["height"] = 150
 		return chart
