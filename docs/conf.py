548e62361dc64737c937180401b18d25886242bb
Doris Lee <dorisjunglinlee@gmail.com>
2020-02-04 10:12:41 -0800

    changed display function to use luxWidget, throws exception if lux-widget not installed; reran demo with luxWidget; bump version
    Former-commit-id: fa3adef3455c099ef9aafb280b1fbb644a097434

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Doris Jung-Lin Lee'
 author = 'Doris Jung-Lin Lee'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
