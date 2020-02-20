ceb4def40cf726943a7a79ecf6f409e41261d2dc
Doris Lee <dorisjunglinlee@gmail.com>
2020-02-20 13:12:47 -0600

    working version of LuxDataframe and spec * overriding pandas default display _repr_html_ * Note that this advertently affects other print functions like head, tail also

--- lux/context/Spec.py
+++ lux/context/Spec.py
@@ -1,26 +1,18 @@
 class Spec:
-    '''
-    Spec is the object representation of a single unit of the specification.
-    '''
-    def __init__(self, 
-                description, 
-                channel="", 
-                dataType="",
-                dataModel="", 
-                transform="",
-                aggregation="",
-                binning="",
-                exclude=False):
+	'''
+	Spec is the object representation of a single unit of the specification.
+	'''
+	def __init__(self, description, channel="", dataType="",dataModel="", transform="",aggregation="",binning="",exclude=False):
 		# Descriptor
 		self.description = description
-        # Description gets comiled to either an attribute, value, attributeGroup or valueGroup
-        # self.attribute = ""
-        # self.value = ""
-        # self.filterOp = ""
-        # self.attributeGroup = []
-        # self.valueGroup = []
-        # self.type = "Attribute", "Filter", "Group"
-        # self.parseDescription()
+		# Description gets comiled to either an attribute, value, attributeGroup or valueGroup
+		# self.attribute = ""
+		# self.value = ""
+		# self.filterOp = ""
+		# self.attributeGroup = []
+		# self.valueGroup = []
+		# self.type = "Attribute", "Filter", "Group"
+		# self.parseDescription()
 		# Properties
 		self.channel = channel
 		self.dataType = dataType
@@ -28,22 +20,10 @@ class Spec:
 		self.transform = transform
 		self.aggregation = aggregation
 		self.binning = binning
-        self.exclude = exclude
-	# def parseDescription():
-    #     # Description gets compiled to either an attribute, value, attributeGroup or valueGroup
-    #     if type(description)==list:
-    #         # set the self.type as "Group"
-    #         # check if this is an attribute
-    #         # if so set it as self.attributeGroup 
-    #     elif type(description)==str:
-    #         # Check if this is an attribute
-    #         # if so set the self.type as "Attribute"
-    #         # Otherwise check if this is a value
-    #         # if so set the self.type as "Value"
-
-            
+		self.exclude = exclude
+			
 	def __repr__(self):
-		repr =  f"Spec <{str(self.columnName)},"+ \
+		repr =  f"Spec <{str(self.description)},"+ \
 			   f"channel:{str(self.channel)},"+ \
 			   f"dataType:{str(self.dataType)},"+ \
 			   f"dataModel:{str(self.dataModel)}>"
