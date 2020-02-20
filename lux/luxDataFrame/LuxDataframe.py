2b92550659fba66faa1fc17fbff6ab174a7da1a2
Doris Lee <dorisjunglinlee@gmail.com>
2020-02-20 13:12:47 -0600

    working version of LuxDataframe and spec * overriding pandas default display _repr_html_ * Note that this advertently affects other print functions like head, tail also
    Former-commit-id: ceb4def40cf726943a7a79ecf6f409e41261d2dc

--- lux/luxDataFrame/LuxDataframe.py
+++ lux/luxDataFrame/LuxDataframe.py
@@ -1,8 +1,4 @@
 import pandas as pd
-global originalDF;
-
-originalDF = pd.core.frame.DataFrame
-
 class LuxDataFrame(pd.DataFrame):
     # normal properties
     _metadata = ['context','spec']
@@ -16,10 +12,31 @@ class LuxDataFrame(pd.DataFrame):
     def _constructor(self):
         return LuxDataFrame
 
-    def indicate(self,luxThing):
-        self.context.append(luxThing)
-
-
+    def setContext(self,context):
+        self.context = context
+    def addToContext(self,context): 
+        self.context.extend(context)
+    def getContext(self):
+        return self.context
+    def __repr__(self):
+        # TODO: _repr_ gets called from _repr_html, need to get rid of this call
+        return ""
+    def _repr_html_(self):
+        import luxWidget
+        import json
+        widgetJSON = json.load(open("mockWidgetJSON.json",'r'))
+        widget = luxWidget.LuxWidget(
+            currentView=widgetJSON["currentView"],
+            recommendations=widgetJSON["recommendations"]
+        )
+        # return widget
+        from IPython.display import display
+        display(widget)
+        
+import pandas as pd
+global originalDF;
+# Keep variable scope of original pandas df
+originalDF = pd.core.frame.DataFrame
 def delegate(bool):
     if bool:
         pd.DataFrame = pd.io.parsers.DataFrame = pd.core.frame.DataFrame = LuxDataFrame
