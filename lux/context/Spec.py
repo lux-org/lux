5c6cd0cc94bb126a9b07622146321f43f679ef63
jaywoo123 <jkim475@illinois.edu>
2020-02-25 14:52:13 -0600

    adding validator

--- lux/context/Spec.py
+++ lux/context/Spec.py
@@ -2,25 +2,25 @@ class Spec:
 	'''
 	Spec is the object representation of a single unit of the specification.
 	'''
-	def __init__(self, description, channel="", dataType="",dataModel="", transform="",aggregation="",binning="",exclude=False):
+	def __init__(self, description="",attribute="",value="",filterOp="",attributeGroup=[],valueGroup=[]):
 		# Descriptor
 		self.description = description
 		# Description gets comiled to either an attribute, value, attributeGroup or valueGroup
-		# self.attribute = ""
-		# self.value = ""
-		# self.filterOp = ""
-		# self.attributeGroup = []
-		# self.valueGroup = ["USA","Japan", ...]
-		# self.type = "Attribute", "Filter", "Group"
+		self.attribute = attribute
+		self.value = value
+		self.filterOp = filterOp
+		self.attributeGroup = attributeGroup
+		self.valueGroup = valueGroup
+		# self.type = "attribute", "value", "attributeGroup", "valueGroup"
 		# self.parseDescription()
 		# Properties
-		self.channel = channel
-		self.dataType = dataType
-		self.dataModel = dataModel
-		self.transform = transform
-		self.aggregation = aggregation
-		self.binning = binning
-		self.exclude = exclude
+		# self.channel = channel
+		# self.dataType = dataType
+		# self.dataModel = dataModel
+		# self.transform = transform
+		# self.aggregation = aggregation
+		# self.binning = binning
+		# self.exclude = exclude
 			
 	def __repr__(self):
 		repr =  f"Spec <{str(self.description)},"+ \
