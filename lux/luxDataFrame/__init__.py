a88f78bca88b760803df6c43f6a0830119cb5c1b
jaywoo123 <jkim475@illinois.edu>
2020-02-26 15:34:56 -0600

    adding init file

--- lux/luxDataFrame/__init__.py
+++ lux/luxDataFrame/__init__.py
@@ -4,10 +4,10 @@ global originalDF;
 # Keep variable scope of original pandas df
 originalDF = pd.core.frame.DataFrame
 
-def delegate(bool):
+def setOption(overridePandas=bool):
     if bool:
         pd.DataFrame = pd.io.parsers.DataFrame = pd.core.frame.DataFrame = LuxDataFrame
     else:
         pd.DataFrame = pd.io.parsers.DataFrame = pd.core.frame.DataFrame = originalDF
 
-delegate(True)
+setOption(overridePandas=True)
