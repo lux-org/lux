e2423515560668f9e3620235299b583c7d35c25a
jrdzha <jaredzhao@berkeley.edu>
2020-03-20 15:59:33 -0700

    pandas tests (some not working) and some basic tests (doesnt include amazon tests)

---
+++ tests/pandas_tests/indexes/timedeltas/test_timedelta_range.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pytest
+
+from pandas import timedelta_range, to_timedelta
+import pandas._testing as tm
+
+from pandas.tseries.offsets import Day, Second
+
+
+class TestTimedeltas:
+    def test_timedelta_range(self):
+
+        expected = to_timedelta(np.arange(5), unit="D")
+        result = timedelta_range("0 days", periods=5, freq="D")
+        tm.assert_index_equal(result, expected)
+
+        expected = to_timedelta(np.arange(11), unit="D")
+        result = timedelta_range("0 days", "10 days", freq="D")
+        tm.assert_index_equal(result, expected)
+
+        expected = to_timedelta(np.arange(5), unit="D") + Second(2) + Day()
+        result = timedelta_range("1 days, 00:00:02", "5 days, 00:00:02", freq="D")
+        tm.assert_index_equal(result, expected)
+
+        expected = to_timedelta([1, 3, 5, 7, 9], unit="D") + Second(2)
+        result = timedelta_range("1 days, 00:00:02", periods=5, freq="2D")
+        tm.assert_index_equal(result, expected)
+
+        expected = to_timedelta(np.arange(50), unit="T") * 30
+        result = timedelta_range("0 days", freq="30T", periods=50)
+        tm.assert_index_equal(result, expected)
+
+    @pytest.mark.parametrize(
+        "periods, freq", [(3, "2D"), (5, "D"), (6, "19H12T"), (7, "16H"), (9, "12H")]
+    )
+    def test_linspace_behavior(self, periods, freq):
+        # GH 20976
+        result = timedelta_range(start="0 days", end="4 days", periods=periods)
+        expected = timedelta_range(start="0 days", end="4 days", freq=freq)
+        tm.assert_index_equal(result, expected)
+        assert result.freq == freq
+
+    def test_errors(self):
+        # not enough params
+        msg = (
+            "Of the four parameters: start, end, periods, and freq, "
+            "exactly three must be specified"
+        )
+        with pytest.raises(ValueError, match=msg):
+            timedelta_range(start="0 days")
+
+        with pytest.raises(ValueError, match=msg):
+            timedelta_range(end="5 days")
+
+        with pytest.raises(ValueError, match=msg):
+            timedelta_range(periods=2)
+
+        with pytest.raises(ValueError, match=msg):
+            timedelta_range()
+
+        # too many params
+        with pytest.raises(ValueError, match=msg):
+            timedelta_range(start="0 days", end="5 days", periods=10, freq="H")
---
+++ tests/pandas_tests/test_downstream.py
@@ -0,0 +1,149 @@
+"""
+Testing that we work in the downstream packages
+"""
+import importlib
+import subprocess
+import sys
+
+import numpy as np  # noqa
+import pytest
+
+from pandas import DataFrame
+import pandas._testing as tm
+
+
+def import_module(name):
+    # we *only* want to skip if the module is truly not available
+    # and NOT just an actual import error because of pandas changes
+
+    try:
+        return importlib.import_module(name)
+    except ModuleNotFoundError:  # noqa
+        pytest.skip(f"skipping as {name} not available")
+
+
+@pytest.fixture
+def df():
+    return DataFrame({"A": [1, 2, 3]})
+
+
+def test_dask(df):
+
+    toolz = import_module("toolz")  # noqa
+    dask = import_module("dask")  # noqa
+
+    import dask.dataframe as dd
+
+    ddf = dd.from_pandas(df, npartitions=3)
+    assert ddf.A is not None
+    assert ddf.compute() is not None
+
+
+@pytest.mark.filterwarnings("ignore:Panel class is removed")
+def test_xarray(df):
+
+    xarray = import_module("xarray")  # noqa
+
+    assert df.to_xarray() is not None
+
+
+def test_oo_optimizable():
+    # GH 21071
+    subprocess.check_call([sys.executable, "-OO", "-c", "import pandas"])
+
+
+@tm.network
+# Cython import warning
+@pytest.mark.filterwarnings("ignore:can't:ImportWarning")
+@pytest.mark.filterwarnings(
+    # patsy needs to update their imports
+    "ignore:Using or importing the ABCs from 'collections:DeprecationWarning"
+)
+def test_statsmodels():
+
+    statsmodels = import_module("statsmodels")  # noqa
+    import statsmodels.api as sm
+    import statsmodels.formula.api as smf
+
+    df = sm.datasets.get_rdataset("Guerry", "HistData").data
+    smf.ols("Lottery ~ Literacy + np.log(Pop1831)", data=df).fit()
+
+
+# Cython import warning
+@pytest.mark.filterwarnings("ignore:can't:ImportWarning")
+def test_scikit_learn(df):
+
+    sklearn = import_module("sklearn")  # noqa
+    from sklearn import svm, datasets
+
+    digits = datasets.load_digits()
+    clf = svm.SVC(gamma=0.001, C=100.0)
+    clf.fit(digits.data[:-1], digits.target[:-1])
+    clf.predict(digits.data[-1:])
+
+
+# Cython import warning and traitlets
+@tm.network
+@pytest.mark.filterwarnings("ignore")
+def test_seaborn():
+
+    seaborn = import_module("seaborn")
+    tips = seaborn.load_dataset("tips")
+    seaborn.stripplot(x="day", y="total_bill", data=tips)
+
+
+def test_pandas_gbq(df):
+
+    pandas_gbq = import_module("pandas_gbq")  # noqa
+
+
+@pytest.mark.xfail(reason="0.7.0 pending")
+@tm.network
+def test_pandas_datareader():
+
+    pandas_datareader = import_module("pandas_datareader")  # noqa
+    pandas_datareader.DataReader("F", "quandl", "2017-01-01", "2017-02-01")
+
+
+# importing from pandas, Cython import warning
+@pytest.mark.filterwarnings("ignore:can't resolve:ImportWarning")
+def test_geopandas():
+
+    geopandas = import_module("geopandas")  # noqa
+    fp = geopandas.datasets.get_path("naturalearth_lowres")
+    assert geopandas.read_file(fp) is not None
+
+
+# Cython import warning
+@pytest.mark.filterwarnings("ignore:can't resolve:ImportWarning")
+@pytest.mark.filterwarnings("ignore:RangeIndex.* is deprecated:DeprecationWarning")
+def test_pyarrow(df):
+
+    pyarrow = import_module("pyarrow")  # noqa
+    table = pyarrow.Table.from_pandas(df)
+    result = table.to_pandas()
+    tm.assert_frame_equal(result, df)
+
+
+@pytest.mark.xfail(reason="pandas-wheels-50", strict=False)
+def test_missing_required_dependency():
+    # GH 23868
+    # To ensure proper isolation, we pass these flags
+    # -S : disable site-packages
+    # -s : disable user site-packages
+    # -E : disable PYTHON* env vars, especially PYTHONPATH
+    # And, that's apparently not enough, so we give up.
+    # https://github.com/MacPython/pandas-wheels/pull/50
+    call = ["python", "-sSE", "-c", "import pandas"]
+
+    msg = (
+        r"Command '\['python', '-sSE', '-c', 'import pandas'\]' "
+        "returned non-zero exit status 1."
+    )
+
+    with pytest.raises(subprocess.CalledProcessError, match=msg) as exc:
+        subprocess.check_output(call, stderr=subprocess.STDOUT)
+
+    output = exc.value.stdout.decode()
+    for name in ["numpy", "pytz", "dateutil"]:
+        assert name in output
