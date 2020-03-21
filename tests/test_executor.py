740da0dfac19f758cbdb444619eb71c8fcdaa9c1
Doris Lee <dorisjunglinlee@gmail.com>
2020-03-21 14:20:46 +0800

    execution engine aggregation working
    Former-commit-id: 62543ed68b29eeb291c629c9658fc00344c0784e

--- tests/test_executor.py
+++ tests/test_executor.py
@@ -12,6 +12,22 @@ def test_selection():
     assert all([type(vc.data)==lux.luxDataFrame.LuxDataframe.LuxDataFrame for vc in df.viewCollection])
     assert all(df.viewCollection[2].data.columns ==["Year",'Acceleration'])
 
+def test_aggregation():
+    df = pd.read_csv("lux/data/car.csv")
+    df.setContext([lux.Spec(attribute = "Horsepower",aggregation="mean"),lux.Spec(attribute = "Origin")])
+    ExecutionEngine.execute(df.viewCollection,df)
+    resultDf = df.viewCollection[0].data
+    assert int(resultDf[resultDf["Origin"]=="USA"]["Horsepower"])==119
+
+    df.setContext([lux.Spec(attribute = "Horsepower",aggregation="sum"),lux.Spec(attribute = "Origin")])
+    ExecutionEngine.execute(df.viewCollection,df)
+    resultDf = df.viewCollection[0].data
+    assert int(resultDf[resultDf["Origin"]=="Japan"]["Horsepower"])==6307
+
+    df.setContext([lux.Spec(attribute = "Horsepower",aggregation="max"),lux.Spec(attribute = "Origin")])
+    ExecutionEngine.execute(df.viewCollection,df)
+    resultDf = df.viewCollection[0].data
+    assert int(resultDf[resultDf["Origin"]=="Europe"]["Horsepower"])==133
 def test_filter():
     df = pd.read_csv("lux/data/car.csv")
     df["Year"] = pd.to_datetime(df["Year"], format='%Y') # change pandas dtype for the column "Year" to datetype
