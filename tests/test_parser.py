d3812dd728579a4cef976cab4aacbe76ca85ab94
19thyneb <thyne.boonmark@gmail.com>
2020-03-29 22:02:42 -0700

    Added more Parser functionality, Bugfix in Validator
    Former-commit-id: 39384795f75814bf33f4c7f5b08e9441af978885

---
+++ tests/test_parser.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import lux
+
+def test_case1():
+	ldf = pd.read_csv("lux/data/car.csv")
+	ldf.setContext(["Horsepower"])
+	assert(type(ldf.context[0]) is lux.Spec)
+	assert(ldf.context[0].attribute == ["Horsepower"])
+	assert(ldf.context[0].type == "attribute")
+	assert(ldf.context[0].attribute == "Horsepower")
+
+def test_case2():
+	ldf = pd.read_csv("lux/data/car.csv")
+	ldf.setContext(["Horsepower", lux.Spec("MilesPerGal",channel="x")])
+	assert(type(ldf.context[0]) is lux.Spec)
+	assert(ldf.context[0].attribute == ["Horsepower"])
+	assert(ldf.context[0].type == "attribute")
+	assert(ldf.context[0].attribute == "Horsepower")
+	assert(type(ldf.context[1]) is lux.Spec)
+	assert(ldf.context[1].attribute == ["MilesPerGal"])
+	assert(ldf.context[1].type == "attribute")
+	assert(ldf.context[1].attribute == "MilesPerGal")
+
+def test_case3():
+	ldf = pd.read_csv("lux/data/car.csv")
+	ldf.setContext(["Horsepower", "Origin=USA"])
+	assert(type(ldf.context[0]) is lux.Spec)
+	assert(ldf.context[0].attribute == ["Horsepower"])
+	assert(ldf.context[0].type == "attribute")
+	assert(ldf.context[0].attribute == "Horsepower")
+	assert(type(ldf.context[1]) is lux.Spec)
+	assert(ldf.context[1].attribute == ["Origin"])
+	assert(ldf.context[1].type == "value")
+	assert(ldf.context[1].value == "USA")
+
+def test_case4():
+	ldf = pd.read_csv("lux/data/car.csv")
+	ldf.setContext(["Horsepower", "Origin=USA/Japan"])
+	assert(type(ldf.context[0]) is lux.Spec)
+	assert(ldf.context[0].attribute == ["Horsepower"])
+	assert(ldf.context[0].type == "attribute")
+	assert(ldf.context[0].attribute == "Horsepower")
+	assert(type(ldf.context[1]) is lux.Spec)
+	assert(ldf.context[1].attribute == ["Origin"])
+	assert(ldf.context[1].type == "valueGroup")
+	assert(ldf.context[1].value == ["USA","Japan"])
+
+def test_case5():
+	ldf = pd.read_csv("lux/data/car.csv")
+	ldf.setContext([["Horsepower", "MilesPerGal", "Weight"], "Origin=USA"])
+	assert(type(ldf.context[0]) is lux.Spec)
+	assert(ldf.context[0].attribute == ["Horsepower", "MilesPerGal", "Weight"])
+	assert(ldf.context[0].type == "attributeGroup")
+	assert(type(ldf.context[1]) is lux.Spec)
+	assert(ldf.context[1].attribute == ["Origin"])
+	assert(ldf.context[1].type == "value")
+	assert(ldf.context[1].value == "USA")
+
+	ldf.setContext(["Horsepower/MilesPerGal/Weight", "Origin=USA"])
+	assert(type(ldf.context[0]) is lux.Spec)
+	assert(ldf.context[0].attribute == ["Horsepower", "MilesPerGal", "Weight"])
+	assert(ldf.context[0].type == "attributeGroup")
+	assert(type(ldf.context[1]) is lux.Spec)
+	assert(ldf.context[1].attribute == ["Origin"])
+	assert(ldf.context[1].type == "value")
+	assert(ldf.context[1].value == "USA")
