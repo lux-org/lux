2de4d509c56ba9104d798f3e1ecaebfc8d2d302e
jrdzha <jaredzhao@berkeley.edu>
2020-03-30 14:15:45 -0700

    finished interestingness
    Former-commit-id: 96a42d30c67c1197624b7b524dab83c6335516c2

--- jared_test_file.py
+++ jared_test_file.py
@@ -1,16 +1,36 @@
 import lux
 import pandas as pd
 
+from lux.interestingness.interestingness import interestingness
+
 
 df = pd.read_csv("lux/data/car.csv")
 df["Year"] = pd.to_datetime(df["Year"], format='%Y') # change pandas dtype for the column "Year" to datetype
 # df.setContext([lux.Spec(attribute = "Horsepower"),lux.Spec(attribute = "Year"), lux.Spec(attribute = "Origin", filterOp="=",value = "USA")])
+
+
+
+# 0 1 0
 df.setContext([lux.Spec(attribute = "Horsepower")])
-from lux.interestingness.interestingness import interestingness
-interestingness(df.viewCollection[0],df)
+print(interestingness(df.viewCollection[0],df))
 
+# 1 1 1
+df.setContext([lux.Spec(attribute = "Horsepower"), lux.Spec(attribute = "Origin", filterOp="=",value = "USA")])
+print(interestingness(df.viewCollection[0],df))
+
+# 1 1 1 set bin size
+df.setContext([lux.Spec(attribute = "Horsepower"), lux.Spec(attribute = "Origin", filterOp="=",value = "USA", binSize=20)])
+print(interestingness(df.viewCollection[0],df))
+
+# 1 1 0
 df.setContext([lux.Spec(attribute = "Horsepower"),lux.Spec(attribute = "Year")])
-interestingness(df.viewCollection[0],df)
+print(interestingness(df.viewCollection[0],df))
 
-# df.showMore()
-# ExecutionEngine.execute(df.viewCollection,df)
+# 0 2 0
+df.setContext([lux.Spec(attribute = "Horsepower"),lux.Spec(attribute = "Acceleration")])
+print(interestingness(df.viewCollection[0],df))
+
+# 0 2 1
+df.setContext([lux.Spec(attribute = "Horsepower"),lux.Spec(attribute = "Acceleration", filterOp=">",value = 10)])
+print(interestingness(df.viewCollection[0],df))
+
