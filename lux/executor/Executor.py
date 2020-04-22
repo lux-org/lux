3bd6e44859ec0ffe99d8aa4e1f8626fb12baad28
Doris Lee <dorisjunglinlee@gmail.com>
2020-04-22 21:33:47 +0800

    cleaned up Executor
    Former-commit-id: 8bffacabbe2ae92dcecb4861db7d1a43dadb4012

--- lux/executor/Executor.py
+++ lux/executor/Executor.py
@@ -8,76 +8,15 @@ class Executor:
         return f"<Executor>"
     @staticmethod
     def execute(viewCollection:ViewCollection, ldf):
-        '''
-        Given a ViewCollection, fetch the data required to render the view
-        1) Apply filters
-        2) Retreive relevant attribute
-        3) return a DataFrame with relevant results
-        '''
-        for view in viewCollection:
-            Executor.executeFilter(view, ldf)
-            # Select relevant data based on attribute information
-            attributes = set([])
-            for spec in view.specLst:
-                if (spec.attribute):
-                    if (spec.attribute=="Record"):
-                        if ('index' not in view.data.columns):
-                            view.data.reset_index(level=0, inplace=True)
-                        attributes.add("index")
-                    else:
-                        attributes.add(spec.attribute)
-            view.data = view.data[list(attributes)]
-            if (view.mark =="bar" or view.mark =="line"):
-                Executor.executeAggregate(view, ldf)
-            elif (view.mark =="histogram"):
-                Executor.executeBinning(view, ldf)
+        return NotImplemented
 
     @staticmethod
     def executeAggregate(view, ldf):
-        # TODO (Jaywoo)
-        # get attribute
-        # aggreagte in spec
-        # horsepower by origin -> lux.spec(horsepower,aggregate = "mean") lux.spec(attribute = Origin)
-        # need to add aggregate spec in the compiling stage(inside compiler.determinEncoding)
-        xAttr = view.getObjFromChannel("x")[0]
-        yAttr = view.getObjFromChannel("y")[0]
-        
-        groupbyAttr =""
-        measureAttr =""
-        if (yAttr.aggregation!=""):
-            groupbyAttr = xAttr
-            measureAttr = yAttr
-            aggFunc = yAttr.aggregation
-        if (xAttr.aggregation!=""):
-            groupbyAttr = yAttr
-            measureAttr = xAttr
-            aggFunc = xAttr.aggregation
-        
-        if (measureAttr!=""):
-            if (measureAttr.attribute=="Record"):
-                countSeries = view.data.groupby(groupbyAttr.attribute).count().iloc[:,0]
-                countSeries.name = "Record"
-                view.data = countSeries.to_frame().reset_index()
-            else:
-                groupbyResult = view.data.groupby(groupbyAttr.attribute)
-                view.data = groupbyResult.agg(aggFunc).reset_index()
+        return NotImplemented
     @staticmethod
     def executeBinning(view, ldf):
-        import numpy as np
-        import pandas as pd # is this import going to be conflicting with LuxDf?
-        binAttribute = list(filter(lambda x: x.binSize!=0,view.specLst))[0]
-        counts,binEdges = np.histogram(ldf[binAttribute.attribute],bins=binAttribute.binSize)
-        #binEdges of size N+1, so need to compute binCenter as the bin location
-        binCenter = np.mean(np.vstack([binEdges[0:-1],binEdges[1:]]), axis=0)
-        # TODO: Should view.data be a LuxDataFrame or a Pandas DataFrame?
-        view.data = pd.DataFrame(np.array([binCenter,counts]).T,columns=[binAttribute.attribute, "Count of Records (binned)"])        
+        return NotImplemented
         
     @staticmethod
     def executeFilter(view, ldf):
-        filters = utils.getFilterSpecs(view.specLst)
-        if (filters):
-            for filter in filters:
-                view.data = ldf[ldf[filter.attribute] == filter.value]
-        else:
-            view.data = ldf
-    
+        return NotImplemented
