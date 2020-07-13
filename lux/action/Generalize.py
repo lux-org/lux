5558206cb3946a09cac66327dab36e2ed2b1dc81
Doris Lee <dorisjunglinlee@gmail.com>
2020-07-13 13:47:19 +0800

    cleanup generalize action to sort based on custom ordering, bugfix to support inequality filters

--- lux/action/Generalize.py
+++ lux/action/Generalize.py
@@ -34,7 +34,7 @@ def generalize(ldf):
 	output = []
 	excluded_columns = []
 	column_spec = list(filter(lambda x: x.value=="" and x.attribute!="Record", ldf.context))
-	row_specs = utils.get_filter_specs(ldf.context)
+	filter_specs = utils.get_filter_specs(ldf.context)
 	# if we do no have enough column attributes or too many, return no views.
 	if(len(column_spec)<2 or len(column_spec)>4):
 		recommendation["collection"] = []
@@ -46,33 +46,33 @@ def generalize(ldf):
 		if type(columns) == list:
 			for column in columns:
 				if column not in excluded_columns:
-					temp_view = View(ldf.context.copy())
+					temp_view = View(ldf.context.copy(),score=1)
 					temp_view.remove_column_from_spec(column, remove_first = False)
 					excluded_columns.append(column)
 					output.append(temp_view)
 		elif type(columns) == str:
 			if columns not in excluded_columns:
-				temp_view = View(ldf.context.copy())
+				temp_view = View(ldf.context.copy(),score=1)
 				temp_view.remove_column_from_spec(columns, remove_first = False)
-				print(temp_view)
 				excluded_columns.append(columns)
 		output.append(temp_view)
 	#for each filter specification, create a copy of the ldf's current view and remove the filter specification,
 	#then append the view to the output
-	for spec in row_specs:
+	for spec in filter_specs:
 		#new_spec = ldf.context.copy()
 		#new_spec.remove_column_from_spec(new_spec.attribute)
-		temp_view = View(ldf.current_view[0].spec_lst.copy())
+		temp_view = View(ldf.current_view[0].spec_lst.copy(),title="Overall",score=0)
 		temp_view.remove_filter_from_spec(spec.value)
 		output.append(temp_view)
 
 	vc = lux.view.ViewCollection.ViewCollection(output)
 	vc = vc.load(ldf)
-	recommendation["collection"] = vc
-	for view in vc:
-		view.score = interestingness(view,ldf)
-	vc.sort(remove_invalid=True)
+	# Ignore interestingness sorting since Generalize yields very few vis (preserve order of remove attribute, then remove filters)
+	# for view in vc:
+	# 	view.score = interestingness(view,ldf)
 	vc.remove_duplicates()
+	vc.sort(remove_invalid=True)
+	recommendation["collection"] = vc
 	#for benchmarking
 	if ldf.toggle_benchmarking == True:
 		toc = time.perf_counter()
