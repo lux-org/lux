08a9704dec0f67c52273e43f2efec39aa0e250f3
Doris Lee <dorisjunglinlee@gmail.com>
2020-07-14 18:32:33 +0800

    readd action to change capitalization
    Former-commit-id: 211d6ff16a1b6d8a96c7f14ccd4d2b29425a4ee5

---
+++ lux/action/generalize.py
@@ -0,0 +1,79 @@
+import lux
+import scipy.stats
+import numpy as np
+from lux.vis.Vis import Vis
+from lux.compiler.Compiler import Compiler
+from lux.utils import utils
+from lux.interestingness.interestingness import interestingness
+
+#for benchmarking
+import time
+# from compiler.Compiler import Compiler
+def generalize(ldf):
+	#for benchmarking
+	if ldf.toggle_benchmarking == True:
+		tic = time.perf_counter()
+	'''
+	Generates all possible visualizations when one attribute or filter from the current view is removed.
+
+	Parameters
+	----------
+	ldf : lux.luxDataFrame.LuxDataFrame
+		LuxDataFrame with underspecified context.
+
+	Returns
+	-------
+	recommendations : Dict[str,obj]
+		object with a collection of visualizations that result from the Generalize action.
+	'''
+	# takes in a dataObject and generates a list of new dataObjects, each with a single measure from the original object removed
+	# -->  return list of dataObjects with corresponding interestingness scores
+
+	recommendation = {"action":"Generalize",
+						   "description":"Remove one attribute or filter to observe a more general trend."}
+	output = []
+	excluded_columns = []
+	column_spec = list(filter(lambda x: x.value=="" and x.attribute!="Record", ldf.context))
+	filter_specs = utils.get_filter_specs(ldf.context)
+	# if we do no have enough column attributes or too many, return no views.
+	if(len(column_spec)<2 or len(column_spec)>4):
+		recommendation["collection"] = []
+		return recommendation
+	#for each column specification, create a copy of the ldf's view and remove the column specification
+	#then append the view to the output
+	for spec in column_spec:
+		columns = spec.attribute
+		if type(columns) == list:
+			for column in columns:
+				if column not in excluded_columns:
+					temp_view = Vis(ldf.context.copy(),score=1)
+					temp_view.remove_column_from_spec(column, remove_first = False)
+					excluded_columns.append(column)
+					output.append(temp_view)
+		elif type(columns) == str:
+			if columns not in excluded_columns:
+				temp_view = Vis(ldf.context.copy(),score=1)
+				temp_view.remove_column_from_spec(columns, remove_first = False)
+				excluded_columns.append(columns)
+		output.append(temp_view)
+	#for each filter specification, create a copy of the ldf's current view and remove the filter specification,
+	#then append the view to the output
+	for spec in filter_specs:
+		#new_spec = ldf.context.copy()
+		#new_spec.remove_column_from_spec(new_spec.attribute)
+		temp_view = Vis(ldf.current_view[0].spec_lst.copy(),ldf,title="Overall",score=0)
+		temp_view.remove_filter_from_spec(spec.value)
+		output.append(temp_view)
+
+	vc = lux.vis.VisCollection.VisCollection(output,ldf)
+	# Ignore interestingness sorting since Generalize yields very few vis (preserve order of remove attribute, then remove filters)
+	# for view in vc:
+	# 	view.score = interestingness(view,ldf)
+	vc.remove_duplicates()
+	vc.sort(remove_invalid=True)
+	recommendation["collection"] = vc
+	#for benchmarking
+	if ldf.toggle_benchmarking == True:
+		toc = time.perf_counter()
+		print(f"Performed generalize action in {toc - tic:0.4f} seconds")
+	return recommendation
---
+++ lux/action/univariate.py
@@ -0,0 +1,55 @@
+from lux.interestingness.interestingness import interestingness
+from lux.vis.VisCollection import VisCollection
+import lux
+#for benchmarking
+import time
+
+def univariate(ldf, data_type_constraint="quantitative"):
+	'''
+	Generates bar chart distributions of different attributes in the dataframe.
+
+	Parameters
+	----------
+	ldf : lux.luxDataFrame.LuxDataFrame
+		LuxDataFrame with underspecified context.
+
+	data_type_constraint: str
+		Controls the type of distribution chart that will be rendered.
+
+	Returns
+	-------
+	recommendations : Dict[str,obj]
+		object with a collection of visualizations that result from the Distribution action.
+	'''
+	import scipy.stats
+	import numpy as np
+
+	#for benchmarking
+	if ldf.toggle_benchmarking == True:
+		tic = time.perf_counter()
+
+	if (data_type_constraint== "quantitative"):
+		query = [lux.VisSpec("?",data_type="quantitative")]
+		query.extend(ldf.filter_specs)
+		recommendation = {"action":"Distribution",
+							"description":"Show histogram distributions of different attributes in the dataframe."}
+	elif (data_type_constraint == "nominal"):
+		query = [lux.VisSpec("?",data_type="nominal")]
+		query.extend(ldf.filter_specs)
+		recommendation = {"action":"Category",
+						   "description":"Show bar charts of different attributes in the dataframe."}
+	elif (data_type_constraint == "temporal"):
+		query = [lux.VisSpec("?",data_type="temporal")]
+		query.extend(ldf.filter_specs)
+		recommendation = {"action":"Temporal",
+						   "description":"Show line chart distributions of time-related attributes in the dataframe."}
+	vc = VisCollection(query,ldf)
+	for view in vc:
+		view.score = interestingness(view,ldf)
+	vc = vc.topK(15)
+	recommendation["collection"] = vc
+	#for benchmarking
+	if ldf.toggle_benchmarking == True:
+		toc = time.perf_counter()
+		print(f"Performed distribution action in {toc - tic:0.4f} seconds")
+	return recommendation
