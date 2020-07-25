f6b3cedeec2d93a70298f773970a7dd5e94f8aa9
Doris Lee <dorisjunglinlee@gmail.com>
2020-07-25 20:59:24 +0800

    added better default styling config for Altair to lessen bolding and align style with rest of widget
    Former-commit-id: 758dfda50655cfbb59130dd15e0dc4c44f37b12d

--- lux/vislib/altair/AltairChart.py
+++ lux/vislib/altair/AltairChart.py
@@ -25,12 +25,26 @@ class AltairChart:
 		# self.add_tooltip()
 		self.encode_color()
 		self.add_title()
+		self.apply_default_config()
 		# ----- END self.code modification -----
 	def __repr__(self):
 		return f"AltairChart <{str(self.view)}>"
 	def add_tooltip(self):
 		if (self.tooltip): 
 			self.chart = self.chart.encode(tooltip=list(self.view.data.columns))
+
+	def apply_default_config(self):
+		self.chart = self.chart.configure_title(fontWeight=500,fontSize=13,font="Helvetica Neue")
+		self.chart = self.chart.configure_axis(titleFontWeight=500,titleFontSize=11,titleFont="Helvetica Neue",
+									labelFontWeight=400,labelFontSize=9,labelFont="Helvetica Neue",labelColor="#505050")
+		self.chart = self.chart.configure_legend(titleFontWeight=500,titleFontSize=10,titleFont="Helvetica Neue",
+									labelFontWeight=400,labelFontSize=9,labelFont="Helvetica Neue")
+		self.code+= "chart = chart.configure_title(fontWeight=500,fontSize=13,font='Helvetica Neue')\n"
+		self.code+= "chart = chart.configure_axis(titleFontWeight=500,titleFontSize=11,titleFont='Helvetica Neue',\n"
+		self.code+= "					labelFontWeight=400,labelFontSize=8,labelFont='Helvetica Neue',labelColor='#505050')\n"
+		self.code+= "chart = chart.configure_legend(titleFontWeight=500,titleFontSize=10,titleFont='Helvetica Neue',\n"
+		self.code+= "					labelFontWeight=400,labelFontSize=8,labelFont='Helvetica Neue')\n"
+
 	def encode_color(self):
 		color_attr = self.view.get_attr_by_channel("color")
 		if (len(color_attr)==1):
@@ -42,7 +56,7 @@ class AltairChart:
 				self.code+=f"chart = chart.encode(color=alt.Color('{color_attr_name}',type='{color_attr_type}',timeUnit='{timeUnit}',title='{color_attr_name}'))"
 			else:
 				self.chart = self.chart.encode(color=alt.Color(color_attr_name,type=color_attr_type))
-				self.code+=f"chart = chart.encode(color=alt.Color('{color_attr_name}',type='{color_attr_type}'))"
+				self.code+=f"chart = chart.encode(color=alt.Color('{color_attr_name}',type='{color_attr_type}'))\n"
 		elif (len(color_attr)>1):
 			raise ValueError("There should not be more than one attribute specified in the same channel.")
 		
