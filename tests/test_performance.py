a9a65b659c0210128c76e02f53e47c0d46757d50
Doris Lee <dorisjunglinlee@gmail.com>
2020-07-27 17:20:57 +0800

    added performance test
    Former-commit-id: a2ddf056a6781da2d8782adb255544169b48fd8d

---
+++ tests/test_performance.py
@@ -0,0 +1,13 @@
+from .context import lux
+import pytest
+import pandas as pd
+import time
+def test_q1_performance_census():
+	df = pd.read_csv("lux/data/census.csv")
+	tic = time.perf_counter()
+	df.set_intent(["education","age"])
+	df._repr_html_()
+	toc = time.perf_counter()
+	delta = toc - tic
+	print(f"Overall Performance: {delta:0.4f} seconds")
+	assert delta < 8, "The recommendations on Census dataset took a total of {delta:0.4f} seconds, longer than expected."
