27714b61d95f09aef2254cf7895a0cb95b198718
Caitlyn Chen <caitlynachen@gmail.com>
2020-08-06 16:45:34 -0700

    extensive tests

--- tests/test_nan.py
+++ tests/test_nan.py
@@ -8,10 +8,26 @@ def test_nan_column():
 	df = pd.read_csv("lux/data/college.csv")
 	df_nan = df
 	df_nan["Geography"]=np.nan
-	# df
-	# df_nan
-	# assert df.recommendation["Occurrence"].__len__() - 1 == df.recommendation["Occurrence"].__len__()
 	for visList in df.recommendation.keys():
 		for vis in df.recommendation[visList]:
-			with pytest.raises(Exception) as e_info:
-				vis.get_attr_by_attr_name("Geography")
+			assert vis.get_attr_by_attr_name("Geography") == []
+			assert vis.get_attr_by_attr_name("PredominantDegree") != []
+			assert vis.get_attr_by_attr_name("ACTMedian") != []
+			assert vis.get_attr_by_attr_name("AverageFacultySalary") != []
+			assert vis.get_attr_by_attr_name("SATAverage") != []
+			assert vis.get_attr_by_attr_name("Expenditure") != []
+			assert vis.get_attr_by_attr_name("Region") != []
+			assert vis.get_attr_by_attr_name("FundingModel") != []
+			assert vis.get_attr_by_attr_name("HighestDegree") != []
+			assert vis.get_attr_by_attr_name("AverageCost") != []
+			assert vis.get_attr_by_attr_name("AverageAgeofEntry") != []
+			assert vis.get_attr_by_attr_name("AdmissionRate") != []
+			assert vis.get_attr_by_attr_name("Name") != []
+
+
+
+
+
+
+
+
