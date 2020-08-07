a66f44ab9b16c4c4e60f92ab47d6faf51c00fb4b
Caitlyn Chen <caitlynachen@gmail.com>
2020-08-06 22:21:52 -0700

    Lux errors when entire column is NaN (#55)
    * Lux errors when entire column is NaN
    
    * revert minor change in LDF
    
    * Refractor logic and add tests for NaN bug
    
    * extensive tests
    
    * resolve minor requested changes

--- lux/action/filter.py
+++ lux/action/filter.py
@@ -25,7 +25,6 @@ def filter(ldf):
 	recommendations : Dict[str,obj]
 		object with a collection of visualizations that result from the Filter action.
 	'''
-	
 	filters = utils.get_filter_specs(ldf._intent)
 	filter_values = []
 	output = []
