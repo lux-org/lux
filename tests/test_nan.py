1e6a7159174b2e1a7492c765e66564609172a535
Doris Lee <dorisjunglinlee@gmail.com>
2020-08-30 20:23:04 +0800

    Expiring metadata in __setitem__ (#63, #81) * Expiring metadata in __setitem__ so that data type and recs recomputed upon typing changes (e.g. when to_datetime is called by the user) * added warning test for checking warning is triggered for temporal detection * fixed tests that inherited from __setitem__ to recompute metadata

--- tests/test_nan.py
+++ tests/test_nan.py
@@ -6,16 +6,8 @@ import numpy as np
 from lux.vis.Vis import Vis
 def test_nan_column():
 	df = pd.read_csv("lux/data/college.csv")
-	df_nan = df
-	df_nan["Geography"]=np.nan
+	df["Geography"] = np.nan
+	df._repr_html_()
 	for visList in df.recommendation.keys():
 		for vis in df.recommendation[visList]:
-			for col in list(filter(lambda x: x!="Geography",df.columns)): assert vis.get_attr_by_attr_name(col)!=[]
-
-
-
-
-
-
-
-
+			assert vis.get_attr_by_attr_name("Geography")==[]
