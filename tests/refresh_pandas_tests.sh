e7df9397b8bd43330fb40d30af9a50300371395c
Doris Lee <dorisjunglinlee@gmail.com>
2020-09-30 23:16:07 +0800

    code cleanup * add all license header * remove utilstest.py,  refresh_pandas_tests.sh * dead code removal

--- tests/refresh_pandas_tests.sh
+++
@@ -1,8 +0,0 @@
-echo ""
-echo "This should be run from within the 'lux/tests' folder"
-echo ""
-rm -rf ./pandas_tests
-rm -rf ./pandas
-git clone https://github.com/pandas-dev/pandas
-cp -R ./pandas/pandas/tests ./pandas_tests
-sudo rm -r pandas
