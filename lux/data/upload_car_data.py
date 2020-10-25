d65687aec259a4956799f6f840db512a9a08ba07
19thyneb <thyne.boonmark@gmail.com>
2020-10-25 10:00:39 -0700

    Added script to generate Postgresql database
    Updated travis.yml file to create postgresql database in test instance.
    
    Added script to populate test database with data.

---
+++ lux/data/upload_car_data.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import psycopg2
+import csv
+
+import psycopg2
+conn = psycopg2.connect("host=localhost dbname=adventureworks user=postgres password=dbadmin")
+cur = conn.cursor()
+cur.execute("""
+	DROP TABLE IF EXISTS car
+	""")
+#create car table in postgres database
+cur.execute("""
+    CREATE TABLE car(
+    name text,
+    milespergal numeric,
+    cylinders integer,
+    displacement numeric,
+    horsepower integer,
+    weight integer,
+    acceleration numeric,
+    year integer,
+    origin text
+)
+""")
+
+#open car.csv and read data into database
+with open('car.csv', 'r') as f:
+	reader = csv.reader(f)
+	next(reader) # Skip the header row.
+	i = 0
+	for row in reader:
+		#print(row)
+		cur.execute(
+		"INSERT INTO car VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
+		row
+	)
+
+conn.commit()
