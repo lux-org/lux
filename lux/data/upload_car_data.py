4a9db883cb863e6378be7026aef722ddb3573534
thyneb19 <thyneboonmark@berkeley.edu>
2020-10-25 18:16:49 -0700

    Added test environment for Postgresql Executor (#124)
    * Merging Recent SQL Executor changes
    
    * Fix to Validator
    
    Uses unique value metadata to verify if a value is valid
    
    * Fix Bug with Widget Rendering
    
    frame.py was trying to import luxWidget instead of luxwidget
    
    * Added Number of Observations to MetaData, Fixed Interestingness issue with SQL Executor
    
    Some interestingness functions required the number of observations in the data and visualization, so I added these values to the metadata to make the scoring work when using the SQL executor
    
    Added tests for SQL executor
    
    * Re-added Licensing Headers
    
    * Adding Recent frame.py changes
    
    * Adjusted SQL Executor Tests
    
    Removed lines that changed Year column type to datetime
    
    * Update Frame with new Action Registering
    
    * Resolving Conflicts in frame.py
    
    * Commenting out local SQL Executor tests
    
    SQL Executor tests interfering with travis build, commenting out for now
    
    * Update correlation.py
    
    * Update frame.py
    
    * Fixing Code Format
    
    * Cleaning up Pandas Executor imports
    
    * Fix Validation Bug
    
    Issue where validator was relying on metadata which was not yet generated, moved metadata calculation before validation step in frame.py
    
    * Changed metadata variable name
    
    Renamed num_obs to length, removed ordinal variable from Executor mapping function
    
    * Added script to generate Postgresql database
    
    Updated travis.yml file to create postgresql database in test instance.
    
    Added script to populate test database with data.
    
    * Update upload_car_data.py
    
    Updated database credentials
    
    * Updated script name in travis.yml
    
    * Removed unnecessary import from travis.yml
    
    * Added psycopg2 to requirements.txt
    
    * Creating Postgres test database in travis
    
    * Fixed directory issue
    
    Co-authored-by: 19thyneb <thyne.boonmark@gmail.com>
    Co-authored-by: Doris Lee <dorisjunglinlee@gmail.com>

---
+++ lux/data/upload_car_data.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import psycopg2
+import csv
+
+import psycopg2
+conn = psycopg2.connect("host=localhost dbname=postgres_db user=postgres password=lux")
+cur = conn.cursor()
+cur.execute("""
+	DROP TABLE IF EXISTS car
+	""")
+#create car table in postgres database
+cur.execute("""
+    CREATE TABLE car(
+    name text,
+    milespergal numeric,
+    cylinders integer,
+    displacement numeric,
+    horsepower integer,
+    weight integer,
+    acceleration numeric,
+    year integer,
+    origin text
+)
+""")
+
+#open car.csv and read data into database
+with open('lux/data/car.csv', 'r') as f:
+	reader = csv.reader(f)
+	next(reader) # Skip the header row.
+	i = 0
+	for row in reader:
+		cur.execute(
+		"INSERT INTO car VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
+		row
+	)
+
+conn.commit()
