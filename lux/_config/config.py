c20726dd9a049ad2b81e2556793c7eac5d29b902
Caitlyn Chen <caitlynachen@berkeley.edu>
2020-11-22 18:28:43 -0800

    fix formatting

--- lux/_config/config.py
+++ lux/_config/config.py
@@ -181,4 +181,3 @@ config = Config()
 
 def warning_format(message, category, filename, lineno, file=None, line=None):
     return "%s:%s: %s:%s\n" % (filename, lineno, category.__name__, message)
-    
