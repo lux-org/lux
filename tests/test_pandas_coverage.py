0e073f07e570952e6a8c448984dcace045b94415
Kunal Agarwal <kagarwal2@berkeley.edu>
2020-11-24 23:42:16 -0800

    fix tests to work with lux-datasets

--- tests/test_pandas_coverage.py
+++ tests/test_pandas_coverage.py
@@ -568,29 +568,29 @@ def test_str_replace(global_var):
 
 
 def test_read_json(global_var):
-    url = "https://github.com/lux-org/lux-datasets/blob/master/data/car.json?raw=true"
-    df = pd.read_csv(url)
+    url = "https://raw.githubusercontent.com/lux-org/lux-datasets/master/data/car.json"
+    df = pd.read_json(url)
     df._repr_html_()
-    assert list(new_df.recommendation.keys()) == [
+    assert list(df.recommendation.keys()) == [
         "Correlation",
         "Distribution",
         "Occurrence",
         "Temporal",
     ]
-    assert len(new_df.data_type_lookup) == 10
+    assert len(df.data_type_lookup) == 10
 
 
 def test_read_html(global_var):
-    url = "https://github.com/lux-org/lux-datasets/blob/master/data/horror.html?raw=true"
+    url = "https://raw.githubusercontent.com/lux-org/lux-datasets/master/data/horror.html"
     df = pd.read_html(url)[1]
     df._repr_html_()
-    assert list(new_df.recommendation.keys()) == ["Occurrence"]
-    assert len(new_df.data_type_lookup) == 5
+    assert list(df.recommendation.keys()) == ["Occurrence"]
+    assert len(df.data_type_lookup) == 5
 
 
 def test_read_sas(global_var):
     url = "https://github.com/lux-org/lux-datasets/blob/master/data/airline.sas7bdat?raw=true"
-    df = pd.read_sas(url)
+    df = pd.read_sas(url, format="sas7bdat")
     df._repr_html_()
-    assert list(new_df.recommendation.keys()) == ["Correlation", "Distribution", "Temporal"]
-    assert len(new_df.data_type_lookup) == 6
+    assert list(df.recommendation.keys()) == ["Correlation", "Distribution", "Temporal"]
+    assert len(df.data_type_lookup) == 6
