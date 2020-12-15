2bb7e721b6429df30a9710f52bc21915824f6173
Doris Lee <dorisjunglinlee@gmail.com>
2020-12-15 17:07:18 +0800

    basic matplotlib chart example

---
+++ lux/vislib/matplotlib/MatplotlibChart.py
@@ -0,0 +1,60 @@
+#  Copyright 2019-2020 The Lux Authors.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import pandas as pd
+from lux.utils.date_utils import compute_date_granularity
+
+
+class MatplotlibChart:
+    """
+    MatplotlibChart is a representation of a chart.
+    Common utilities for charts that is independent of chart types should go here.
+
+    See Also
+    --------
+    https://matplotlib.org/
+
+    """
+
+    def __init__(self, vis):
+        self.vis = vis
+        self.data = vis.data
+        self.tooltip = True
+        # ----- START self.code modification -----
+        self.code = ""
+        self.chart = self.initialize_chart()
+        # self.add_tooltip()
+        # self.encode_color()
+        # self.add_title()
+        # self.apply_default_config()
+
+        # ----- END self.code modification -----
+
+    def __repr__(self):
+        return f"MatplotlibChart <{str(self.vis)}>"
+
+    def add_tooltip(self):
+        return NotImplemented
+
+    def apply_default_config(self):
+        return NotImplemented
+
+    def encode_color(self):
+        return NotImplemented
+
+    def add_title(self):
+        return NotImplemented
+
+    def initialize_chart(self):
+        return NotImplemented
