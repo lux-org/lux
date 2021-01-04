3a27d76179a660651a106ad84f45a0e72979e564
19thyneb <thyne.boonmark@gmail.com>
2021-01-04 10:49:04 -0800

    Black Formatting

--- lux/data/upload_car_data.py
+++ lux/data/upload_car_data.py
@@ -29,10 +29,13 @@ cur.execute(
 
 # open car.csv and read data into database
 import urllib.request
+
 target_url = "https://raw.githubusercontent.com/lux-org/lux-datasets/master/data/car.csv"
 for line in urllib.request.urlopen(target_url):
     decoded = line.decode("utf-8")
     print(decoded.split(","))
-    if 'Name,MilesPerGal,Cylinders' not in decoded:
-        cur.execute("INSERT INTO cars VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", decoded.split(","))
+    if "Name,MilesPerGal,Cylinders" not in decoded:
+        cur.execute(
+            "INSERT INTO cars VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", decoded.split(",")
+        )
 conn.commit()
