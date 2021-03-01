e078d2fe237ea829416b12a81492fa5049cdde23
m p <mantejpanesar@berkeley.edu>
2021-02-28 22:28:53 -0800

    Resolve PR comments

--- lux/action/temporal.py
+++ lux/action/temporal.py
@@ -17,6 +17,9 @@ from lux.vis.VisList import VisList
 from lux.vis.Vis import Vis
 import pandas as pd
 from lux.core.frame import LuxDataFrame
+from lux.utils.date_utils import compute_date_granularity
+from lux.interestingness.interestingness import interestingness
+from lux.utils import utils
 
 
 def temporal(ldf):
@@ -34,28 +37,44 @@ def temporal(ldf):
             Object with a collection of visualizations that result from the Temporal action.
     """
     visuals = []
-    for c in ldf.columns:
-        if ldf.data_type[c] == "temporal":
-            try:
-                generated_vis = create_vis(ldf, c)
-                visuals.extend(generated_vis)
-            except (ValueError, AttributeError):
-                pass
     recommendation = {
         "action": "Temporal",
         "description": "Show trends over <p class='highlight-descriptor'>time-related</p> attributes.",
+        "long_description": "Temporal displays line charts for all attributes related to datetimes in the dataframe.",
     }
+
+    for c in ldf.columns:
+        if ldf.data_type[c] == "temporal":
+            try:
+                generated_vis = create_temporal_vis(ldf, c)
+                visuals.extend(generated_vis)
+            except (ValueError, AttributeError, KeyError):
+                pass
+
+    # If no temporal visualizations were generated via parsing datetime, fallback to default behavior.
+    if len(visuals) == 0:
+        intent = [lux.Clause("?", data_type="temporal")]
+        intent.extend(utils.get_filter_specs(ldf._intent))
+        vlist = VisList(intent, ldf)
+        for vis in vlist:
+            vis.score = interestingness(vis, ldf)
+    else:
+        vlist = VisList(visuals)
+        recommendation["long_description"] += (
+            " The scoring will display all overall visualizations first,"
+            + "all the (year) visualizations second, all the (month) visualizations third, and finally all the (day) visualizations."
+        )
+
     # Doesn't make sense to generate a line chart if there is less than 3 datapoints (pre-aggregated)
     if len(ldf) < 3:
         recommendation["collection"] = []
         return recommendation
-    vlist = VisList(visuals)
     vlist.sort()
     recommendation["collection"] = vlist
     return recommendation
 
 
-def create_vis(ldf, col):
+def create_temporal_vis(ldf, col):
     """
     Creates and populates Vis objects for the different time-scale visualizations of the provided temporal column.
 
@@ -72,38 +91,43 @@ def create_vis(ldf, col):
     visuals : [Vis]
             Collection of Vis objects.
     """
+    formatted_date = pd.to_datetime(ldf[col], format="%Y-%m-%d")
+
+    overall_vis = Vis([lux.Clause(col, data_type="temporal")], source=ldf, score=4)
+
+    year_col = col + " (year)"
+    year_df = LuxDataFrame({year_col: pd.to_datetime(formatted_date.dt.year, format="%Y")})
+    year_vis = Vis([lux.Clause(year_col, data_type="temporal")], source=year_df, score=3)
+
+    month_col = col + " (month)"
+    month_df = LuxDataFrame({month_col: pd.to_datetime(formatted_date.dt.month, format="%m")})
+    month_vis = Vis([lux.Clause(month_col, data_type="temporal")], source=month_df, score=2)
+
+    day_col = col + " (day)"
+    day_df = LuxDataFrame({day_col: pd.to_datetime(formatted_date.dt.day, format="%d")})
+    day_vis = Vis([lux.Clause(day_col, data_type="temporal")], source=day_df, score=1)
+
+    date_granularity = compute_date_granularity(formatted_date)
+    unique_year_values = len(year_df[year_col].unique())
+    unique_month_values = len(month_df[month_col].unique())
     visuals = []
-    converted_string = ldf[col].astype(str)
-    if converted_string.str.contains("-").any():
-        parsed_date = converted_string.str.extract(r"([0-9]{4})?-?([0-9]{2})?-?([0-9]{1,2})?")
-        valid_year, valid_month, valid_day = parsed_date.apply(lambda x: not x.isnull().all()).values
 
-        date_vis = Vis([lux.Clause(col, data_type="temporal")], source=ldf, score=4)
-        visuals.append(date_vis)
-
-        # If date components (year, month, day) were parsed, create individuals visualizations for each
-        if valid_year:
-            year_col = col + " (year)"
-            year_df = LuxDataFrame(
-                {year_col: pd.to_datetime(parsed_date[0], format="%Y").dt.strftime("%Y")}
-            )
-            year_vis = Vis([lux.Clause(year_col, data_type="temporal")], source=year_df, score=3)
+    if date_granularity == "year":
+        visuals.append(overall_vis)
+    elif date_granularity == "month":
+        if unique_year_values != 1:
             visuals.append(year_vis)
-        if valid_month:
-            month_col = col + " (month)"
-            month_df = LuxDataFrame(
-                {month_col: pd.to_datetime(parsed_date[1], format="%m").dt.strftime("%m")}
-            )
-            month_vis = Vis([lux.Clause(month_col, data_type="temporal")], source=month_df, score=2)
+        if len(visuals) != 0:
+            visuals.extend([overall_vis, month_vis])
+        else:
+            visuals.append(overall_vis)
+    elif date_granularity == "day":
+        if unique_year_values != 1:
+            visuals.append(year_vis)
+        if unique_month_values != 1:
             visuals.append(month_vis)
-        if valid_day:
-            day_col = col + " (day)"
-            day_df = LuxDataFrame(
-                {day_col: pd.to_datetime(parsed_date[2], format="%d").dt.strftime("%d")}
-            )
-            day_vis = Vis([lux.Clause(day_col, data_type="temporal")], source=day_df, score=1)
-            visuals.append(day_vis)
-    else:
-        single_vis = Vis([lux.Clause(col, data_type="temporal")], source=ldf, score=5)
-        visuals.append(single_vis)
+        if len(visuals) != 0:
+            visuals.extend([overall_vis, day_vis])
+        else:
+            visuals.append(overall_vis)
     return visuals
