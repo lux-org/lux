28729fb744d1b29178c0374a04e61a1100143b65
Kunal Agarwal <kagarwal2@berkeley.edu>
2021-03-02 16:59:07 -0800

    make parent LuxGroupby

--- lux/core/groupby.py
+++ lux/core/groupby.py
@@ -29,6 +29,7 @@ class LuxGroupBy(pd.core.groupby.groupby.GroupBy):
         super(LuxGroupBy, self).__init__(*args, **kwargs)
 
     def aggregate(self, *args, **kwargs):
+        print("woooo")
         ret_val = super(LuxGroupBy, self).aggregate(*args, **kwargs)
         for attr in self._metadata:
             ret_val.__dict__[attr] = getattr(self, attr, None)
@@ -81,63 +82,11 @@ class LuxGroupBy(pd.core.groupby.groupby.GroupBy):
 
     agg = aggregate
 
-class LuxDataFrameGroupBy(pd.core.groupby.generic.DataFrameGroupBy, LuxGroupBy):
+class LuxDataFrameGroupBy(LuxGroupBy, pd.core.groupby.generic.DataFrameGroupBy):
 
     def __init__(self, *args, **kwargs):
         super(LuxDataFrameGroupBy, self).__init__(*args, **kwargs)
 
-    # def aggregate(self, *args, **kwargs):
-    #     ret_val = super(LuxDataFrameGroupBy, self).aggregate(*args, **kwargs)
-    #     for attr in self._metadata:
-    #         ret_val.__dict__[attr] = getattr(self, attr, None)
-    #     return ret_val
-
-    # def _agg_general(self, *args, **kwargs):
-    #     ret_val = super(LuxDataFrameGroupBy, self)._agg_general(*args, **kwargs)
-    #     for attr in self._metadata:
-    #         ret_val.__dict__[attr] = getattr(self, attr, None)
-    #     return ret_val
-
-    # def _cython_agg_general(self, *args, **kwargs):
-    #     ret_val = super(LuxDataFrameGroupBy, self)._cython_agg_general(*args, **kwargs)
-    #     for attr in self._metadata:
-    #         ret_val.__dict__[attr] = getattr(self, attr, None)
-    #     return ret_val
-
-    # def get_group(self, *args, **kwargs):
-    #     ret_val = super(LuxDataFrameGroupBy, self).get_group(*args, **kwargs)
-    #     for attr in self._metadata:
-    #         ret_val.__dict__[attr] = getattr(self, attr, None)
-    #     ret_val.pre_aggregated = False  # Returned LuxDataFrame isn't pre_aggregated
-    #     return ret_val
-
-    # def filter(self, *args, **kwargs):
-    #     ret_val = super(LuxDataFrameGroupBy, self).filter(*args, **kwargs)
-    #     for attr in self._metadata:
-    #         ret_val.__dict__[attr] = getattr(self, attr, None)
-    #     ret_val.pre_aggregated = False  # Returned LuxDataFrame isn't pre_aggregated
-    #     return ret_val
-
-    # def apply(self, *args, **kwargs):
-    #     ret_val = super(LuxDataFrameGroupBy, self).apply(*args, **kwargs)
-    #     for attr in self._metadata:
-    #         ret_val.__dict__[attr] = getattr(self, attr, None)
-    #     ret_val.pre_aggregated = False  # Returned LuxDataFrame isn't pre_aggregated
-    #     return ret_val
-
-    # def size(self, *args, **kwargs):
-    #     ret_val = super(LuxDataFrameGroupBy, self).size(*args, **kwargs)
-    #     for attr in self._metadata:
-    #         ret_val.__dict__[attr] = getattr(self, attr, None)
-    #     return ret_val
-
-    # def __getitem__(self, *args, **kwargs):
-    #     ret_val = super(LuxDataFrameGroupBy, self).__getitem__(*args, **kwargs)
-    #     for attr in self._metadata:
-    #         ret_val.__dict__[attr] = getattr(self, attr, None)
-    #     return ret_val
-
-    # agg = aggregate
 class LuxSeriesGroupBy(pd.core.groupby.generic.SeriesGroupBy, LuxGroupBy):
 
     def __init__(self, *args, **kwargs):
