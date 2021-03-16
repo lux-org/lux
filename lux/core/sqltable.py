626cc5f0af670b63543797fed9dff186055c9c6d
19thyneb <thyneboonmark@berkeley.edu>
2021-03-15 17:50:33 -0700

    Update Lux SQLTable Frontend
    Added disclaimer for the data preview

--- lux/core/sqltable.py
+++ lux/core/sqltable.py
@@ -109,7 +109,7 @@ class LuxSQLTable(lux.LuxDataFrame):
                 )
 
     def _repr_html_(self):
-        from IPython.display import display
+        from IPython.display import HTML, display
         from IPython.display import clear_output
         import ipywidgets as widgets
 
@@ -143,7 +143,15 @@ class LuxSQLTable(lux.LuxDataFrame):
             )
             self.output = widgets.Output()
             lux.config.executor.execute_preview(self)
-            display(button, self.output)
+            notice = HTML("""<style>
+            body
+            {
+            background-color:#aa0000;
+            }
+            </style>
+            Please note, the data shown here is just a preview of the database table. You will be unable to perform Pandas functionality on this data.""")
+
+            display(button, notice, self.output)
 
             def on_button_clicked(b):
                 with self.output:
