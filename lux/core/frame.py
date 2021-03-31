e32f8ae28221a8315f7899ea5d2942e34af5148b
Caitlyn Chen <caitlynachen@berkeley.edu>
2021-03-31 12:39:05 -0700

    only quant

--- lux/core/frame.py
+++ lux/core/frame.py
@@ -498,12 +498,14 @@ class LuxDataFrame(pd.DataFrame):
                 if len(vlist) > 0:
                     rec_df._recommendation[action_type] = vlist
             rec_df._rec_info = rec_infolist
-            if len(self.columns) == 2 or len(self.columns) == 3:
+            quantitative_columns = [i for i in self.dtypes if i != 'O' and i != 'str']
+            if len(quantitative_columns) == 2 or len(quantitative_columns) == 3:
                 self.current_vis = VisList([i for i in self.columns], self)
             self._widget = rec_df.render_widget()
         # re-render widget for the current dataframe if previous rec is not recomputed
         elif show_prev:
-            if len(self.columns) == 2 or len(self.columns) == 3:
+            quantitative_columns = [i for i in self.dtypes if i != 'O' and i != 'str']
+            if len(quantitative_columns) == 2 or len(quantitative_columns) == 3:
                 self.current_vis = VisList([i for i in self.columns], self)
             self._widget = rec_df.render_widget()
         self._recs_fresh = True
