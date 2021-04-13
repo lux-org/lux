53672ab1dc1a73a77cd94586abdfaea603d44e94
Doris Lee <dorisjunglinlee@gmail.com>
2021-04-13 13:45:19 -0700

    modified tests

--- tests/test_action.py
+++ tests/test_action.py
@@ -277,9 +277,11 @@ def test_intent_retained():
     assert list(df.recommendation.keys()) == ["Enhance", "Filter"]
 
 
-def test_invalid_intent():
+def test_metadata_propogate_invalid_intent():
     df = pd.read_csv("https://raw.githubusercontent.com/lux-org/lux-datasets/master/data/employee.csv")
     df.intent = ["Attrition"]
     new_df = df.groupby("BusinessTravel").mean()
+    assert new_df.intent[0].attribute == "Attrition", "User-specified intent is retained"
+    assert new_df._inferred_intent == [], "Invalid inferred intent is cleared"
     new_df._ipython_display_()
     assert new_df.current_vis == []
