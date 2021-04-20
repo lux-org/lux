25bad5ad746d68b5b03f781e35757a4cf471fb40
Micah Yong <micahtyong@gmail.com>
2021-04-19 18:26:25 -0700

    Clean up diff

--- lux/utils/utils.py
+++ lux/utils/utils.py
@@ -125,7 +125,6 @@ def like_geo(val):
 
 def matplotlib_setup(w, h):
     plt.ioff()
-    scale = lux.config.plotting_scale
     fig, ax = plt.subplots(figsize=(w, h))
     ax.set_axisbelow(True)
     ax.grid(color="#dddddd")
