09b522696953d86b24f8d699790ca665797ee5f5
Acorn <wanglj17@mails.tsinghua.edu.cn>
2021-06-21 15:45:52 +0800

    Log the loc/iloc action to both the parent and child dataframe

--- lux/core/loc.py
+++ lux/core/loc.py
@@ -47,36 +47,15 @@ class LuxLocIndexer(pd.core.indexing._LocIndexer):
             self._parent_df.history.freeze()
             ret_value = super(LuxLocIndexer, self).__getitem__(key)
             self._parent_df.history.unfreeze()
-            if not isinstance(ret_value, LuxSeries) and not isinstance(ret_value, LuxDataFrame):
-                # then only a single value will be returned. 
-                # In this case, we modify the history of the parent dataframe and 
-                # note that here the key should be a two-element tuple 
-                # and the second element is the column the user is intereseted in
-                self._parent_df.history.append_event("loc", [key[1]], rank_type="parent", child_df=ret_value)
+
+            columns = convert_names_to_columns(self._parent_df.columns, key) if type(key) is tuple else []
+            if isinstance(ret_value, LuxSeries) or isinstance(ret_value, LuxDataFrame):
+                ret_value = self._lux_copymd(ret_value)
+                ret_value._parent_df = self._parent_df
+                ret_value.history.append_event("loc", columns, rank_type="child", child_df=None)
+            self._parent_df.history.append_event("loc", columns, rank_type="parent", child_df=ret_value)
         else:
             ret_value = super(LuxLocIndexer, self).__getitem__(key)
-        return ret_value
-
-    def _getitem_axis(self, key, axis: int):
-        ret_value = super(LuxLocIndexer, self)._getitem_axis(key, axis)
-        ret_value = self._lux_copymd(ret_value)
-        ret_value._parent_df = self._parent_df
-        ret_value.history.append_event("loc", [], rank_type="child", child_df=None)
-        return ret_value
-
-    def _getitem_tuple(self, tup: Tuple):
-        ret_value = super(LuxLocIndexer, self)._getitem_tuple(tup)
-        ret_value = self._lux_copymd(ret_value)
-        ret_value._parent_df = self._parent_df
-        if self._parent_df is not None:
-            # _getitem_tuple will be called inside the inner codes of pandas
-            columns = convert_names_to_columns(self._parent_df.columns, tup)
-            # inside the _getitem_tuple the function _getitem_axis might be called
-            # at least when we call df.loc["cobra", ["shield", "max_speed"]]
-            if ret_value.history.check_event(-1, op_name="loc"):
-                ret_value.history.edit_event(-1, "loc", columns, rank_type="child", child_df=None)
-            else:
-                ret_value.history.append_event("loc", columns, rank_type="child", child_df=None)
         return ret_value
     
     def __setitem__(self, key, value):
@@ -84,16 +63,10 @@ class LuxLocIndexer(pd.core.indexing._LocIndexer):
             self._parent_df.history.freeze()
             super(LuxLocIndexer, self).__setitem__(key, value)
             self._parent_df.history.unfreeze()
-            if isinstance(key, tuple):
-                # here some columns must be referenced
-                columns = convert_names_to_columns(self._parent_df.columns, key)
-                if columns is not None: 
-                    # if the key[1] is multi-index instead of list, str, slice, 
-                    # we choose to not log such action for now.
-                    self._parent_df.history.append_event("loc", columns, rank_type="parent", child_df=None)
-            else:
-                # only some rows have been referenced
-                self._parent_df.history.append_event("loc", [], rank_type="parent", child_df=None)
+            columns = convert_names_to_columns(self._parent_df.columns, key) if type(key) is tuple else []
+            if columns is not None: 
+                # if the key[1] is multi-index instead of list, str, slice, we choose to not log such action for now.
+                self._parent_df.history.append_event("loc", columns, rank_type="parent", child_df=None)
         else:
             super(LuxLocIndexer, self).__setitem__(key, value)
     
@@ -141,61 +114,36 @@ class iLuxLocIndexer(pd.core.indexing._iLocIndexer):
     @history.setter
     def history(self, history: History):
         self._history = history
-
+   
     def __getitem__(self, key):
         if self._parent_df is not None:
             self._parent_df.history.freeze()
             ret_value = super(iLuxLocIndexer, self).__getitem__(key)
             self._parent_df.history.unfreeze()
-            if not isinstance(ret_value, LuxSeries) and not isinstance(ret_value, LuxDataFrame):
-                # then only a single value will be returned. 
-                # In this case, we modify the history of the parent dataframe and 
-                # note that here the key should be a two-element tuple 
-                # and the second element is the column the user is intereseted in
-                self._parent_df.history.append_event("iloc", self._parent_df.columns[key[1]], rank_type="parent", child_df=ret_value)
+            
+            columns = convert_indices_to_columns(self._parent_df.columns, key) if type(key) is tuple else []
+            if isinstance(ret_value, LuxSeries) or isinstance(ret_value, LuxDataFrame):
+                ret_value = self._lux_copymd(ret_value)
+                ret_value._parent_df = self._parent_df
+                ret_value.history.append_event("iloc", columns, rank_type="child", child_df=None)
+            self._parent_df.history.append_event("iloc", columns, rank_type="parent", child_df=ret_value)
         else:
             ret_value = super(iLuxLocIndexer, self).__getitem__(key)
         return ret_value
+
     
     def __setitem__(self, key, value):
         if self._parent_df is not None:
             self._parent_df.history.freeze()
             super(iLuxLocIndexer, self).__setitem__(key, value)
             self._parent_df.history.unfreeze()
-            if isinstance(key, tuple):
-                # here some columns must be referenced
-                columns = convert_indices_to_columns(self._parent_df.columns, key)
-                if columns is not None: 
-                    # if the key[1] is multi-index instead of list, int, slice, 
-                    # we choose to not log such action for now.
-                    self._parent_df.history.append_event("iloc", columns, rank_type="parent", child_df=None)
-            else:
-                # only some rows have been referenced
-                self._parent_df.history.append_event("iloc", [], rank_type="parent", child_df=None)
+
+            columns = convert_indices_to_columns(self._parent_df.columns, key) if type(key) is tuple else []
+            if columns is not None: 
+                # if the key[1] is multi-index instead of list, int, slice, we choose to not log such action for now.
+                self._parent_df.history.append_event("iloc", columns, rank_type="parent", child_df=None)
         else:
             super(iLuxLocIndexer, self).__setitem__(key, value)
-
-
-    def _getitem_axis(self, key, axis: int):
-        ret_value = super(iLuxLocIndexer, self)._getitem_axis(key, axis)
-        ret_value = self._lux_copymd(ret_value)
-        ret_value._parent_df = self._parent_df 
-        ret_value.history.append_event("iloc", [], rank_type="child", child_df=None)
-        return ret_value
-
-    def _getitem_tuple(self, tup: Tuple):
-        ret_value = super(iLuxLocIndexer, self)._getitem_tuple(tup)
-        ret_value = self._lux_copymd(ret_value)
-        ret_value._parent_df = self._parent_df
-        if self._parent_df is not None:
-            columns = convert_indices_to_columns(self._parent_df.columns, tup)
-            # inside the _getitem_tuple the function _getitem_axis might be called
-            # at least when we call df.loc["cobra", ["shield", "max_speed"]]
-            if ret_value.history.check_event(-1, op_name="iloc"):
-                ret_value.history.edit_event(-1, "iloc", columns, rank_type="child", child_df=None)
-            else:
-                ret_value.history.append_event("iloc", columns, rank_type="child", child_df=None)
-        return ret_value
 
     def _lux_copymd(self, ret_value):
         for attr in self._metadata:
