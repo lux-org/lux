b73080c5caa65bf2069d864ed5864cc60a1a0406
Acorn <wanglj17@mails.tsinghua.edu.cn>
2021-07-03 20:26:10 +0800

    Copy the parent history object to the child (the bug was caused by the removal of most metadata attributes)

--- lux/core/loc.py
+++ lux/core/loc.py
@@ -77,7 +77,7 @@ class LuxLocIndexer(pd.core.indexing._LocIndexer):
     def _lux_copymd(self, ret_value):
         for attr in self._metadata:
             ret_value.__dict__[attr] = getattr(self._parent_df, attr, None)
-        ret_value.history = ret_value.history.copy()
+        ret_value.history = getattr(self._parent_df, "_history", None).copy()
         return ret_value
 
 class iLuxLocIndexer(pd.core.indexing._iLocIndexer):
@@ -152,5 +152,5 @@ class iLuxLocIndexer(pd.core.indexing._iLocIndexer):
     def _lux_copymd(self, ret_value):
         for attr in self._metadata:
             ret_value.__dict__[attr] = getattr(self._parent_df, attr, None)
-        ret_value.history = ret_value.history.copy()
+        ret_value.history = getattr(self._parent_df, "_history", None).copy()
         return ret_value
