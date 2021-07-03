52bcf80d0a3ec9339f133ee5e53489ed1fe1e7a0
Acorn <wanglj17@mails.tsinghua.edu.cn>
2021-07-03 19:18:05 +0800

    Implement the unit test for df.fillna(0)

--- tests/test_history.py
+++ tests/test_history.py
@@ -16,11 +16,13 @@ from .context import lux
 import pytest
 import pandas as pd
 
-def _check_log(df, op_name, parent_status=None):
+def _check_log(df, op_name, parent_status=None, cols=None):
     assert df.history[-1].op_name == op_name, "The %s() call is not logged to the child dataframe." % op_name
     assert len(df.history) == 1, "Other function calls are logged to the child dataframe unnecessarily."
     if parent_status is not None:
-        assert df.history[-1].kwargs.get("rank_type", None) == parent_status
+        assert df.history[-1].kwargs.get("rank_type", None) == parent_status, "The rank type is supposed to be set as %s" % parent_status 
+    if cols is not None:
+        assert set(df.history[-1].cols) == set(cols), "These columns should be logged as attributes of the event {}".format(cols)
 
 def test_head(global_var):
     df = pytest.car_df.copy(deep=True)
@@ -98,3 +100,27 @@ def test_dropna(global_var):
     _check_log(new_df, "dropna", parent_status="child")
     # parent dataframe
     _check_log(df, "dropna", parent_status="parent")
+
+def test_fillna(global_var):
+    def prepare_NA_df():
+        df = pytest.car_df.copy(deep=True)
+        with df.history.pause(): # the original dataset does no have any na value.
+            df.loc[0, "Miles_per_Gallon"] = None
+            df.loc[1, "Horsepower"] = None
+        return df
+    # test the inplace = False case
+    df = prepare_NA_df()
+    new_df = df.fillna(0)
+    # parent dataframe
+    _check_log(df, "fillna", parent_status="parent", cols=['Miles_per_Gallon', 'Horsepower'])
+    # child dataframe
+    _check_log(new_df, "fillna", parent_status="child", cols=['Miles_per_Gallon', 'Horsepower'])
+
+    # test the inplace = True case
+    df = prepare_NA_df()
+    new_df = df.fillna(0, inplace=True)
+    # parent dataframe
+    _check_log(df, "fillna", parent_status="parent", cols=['Miles_per_Gallon', 'Horsepower'])
+
+
+
