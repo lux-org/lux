3bdc6f1a047b5d68c617f32099ebe1595f10cc5c
Acorn <wanglj17@mails.tsinghua.edu.cn>
2021-07-03 15:50:40 +0800

    Implement the unit test for pd.isnull(df)

--- lux/core/frame.py
+++ lux/core/frame.py
@@ -1121,7 +1121,8 @@ class LuxDataFrame(pd.DataFrame):
         with self.history.pause():
             ret_value = super(LuxDataFrame, self).isnull(*args, **kwargs)
         self.history.append_event("isna", [], rank_type="parent")
-        ret_value.history.delete_at(-1) # isna gets added twice
+        ret_value.history.delete_at(-1) # isna gets added twice 
+        ## is it? It seems that deletion of this sentence does not make a difference here.
         ret_value.history.append_event("isna", [], rank_type="child")
 
         return ret_value
