188a8c533f21508d0516269c89c90a9450e354d3
Cristian Garcia <cgarcia.e88@gmail.com>
2022-02-03 10:08:11 -0500

    add docs + test

--- lux/utils/debug_utils.py
+++ lux/utils/debug_utils.py
@@ -1,13 +1,25 @@
-def show_versions():
+from typing import Optional
+
+
+def show_versions(return_string: bool = False) -> Optional[str]:
     """
     Prints the versions of the principal packages used by Lux for debugging purposes.
+
+    Parameters
+    ----------
+    return_string: Whether to return the versions as a string or print them.
+
+    Returns
+    -------
+    If return_string is True, returns a string with the versions else the versions
+    are printed and None is returned.
     """
     import pandas as pd
     import lux
     import luxwidget
     import matplotlib
     import altair
-import platform
+    import platform
 
     df = pd.DataFrame(
         [
@@ -18,10 +30,15 @@ import platform
             ("matplotlib", matplotlib.__version__),
             ("altair", altair.__version__),
         ],
-        columns=["", "Version"]
+        columns=["", "Version"],
     )
 
-    print(df.to_string(index=False))
+    str_rep = df.to_string(index=False)
+
+    if return_string:
+        return str_rep
+    else:
+        print(str_rep)
 
 
 if __name__ == "__main__":
---
+++ tests/test_utils.py
@@ -0,0 +1,16 @@
+import lux
+
+
+class TestDebugUtils:
+    def test_show_info(self):
+
+        versions = lux.show_versions(return_string=True)
+
+        assert versions is not None
+
+        assert "python" in versions
+        assert "lux" in versions
+        assert "pandas" in versions
+        assert "luxwidget" in versions
+        assert "matplotlib" in versions
+        assert "altair" in versions
